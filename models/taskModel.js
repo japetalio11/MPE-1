49b0b20ececb1db2077843034ac63e1ee70f5999
Pkv562 <pvillamer@gbox.adnu.edu.ph>
2025-10-08 11:08:24 +0800

    [TASK-01] Create Task model with fields: id, title, description, createdBy.

---
+++ models/taskModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String
+  },
+  createdBy: {
+    type: String,
+    required: true
+  }
+}, { timestamps: true });
+
+const Task = mongoose.models.Task || mongoose.model('Task', taskSchema);
+
+module.exports = Task;
+
